f62dde0d896a6d0468d49acbeae6a9487e0cc7ff
thenolanpierce <thenolanpierce@gmail.com>
2024-08-10 02:24:13 -0700

    Developed half of our api :)

--- nucleus-api/src/app.js
+++ nucleus-api/src/app.js
@@ -1,16 +1,34 @@
-// src/app.js
 const express = require('express');
+const userRegisterRoutes = require('./routes/userRegisterRoutes');
+const userRoutes = require('./routes/userRoutes');
+const webLoginRoutes = require('./routes/webLoginRoutes');
+const clientLoginRoutes = require('./routes/clientLoginRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const licenseRoutes = require('./routes/licenseRoutes')
+
+
+
 const app = express();
 
-// Middleware to parse JSON requests
 app.use(express.json());
 
-// Basic route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use('/api', userRegisterRoutes);
 
-// Start the server
+// Use the license routes
+app.use('/api/licenses', licenseRoutes);
+
+// Use the user routes
+app.use('/api', userRoutes);
+
+// Use the web login route
+app.use('/api', webLoginRoutes);
+
+// Use the client login route
+app.use('/api', clientLoginRoutes);
+
+// Use the admin routes
+app.use('/api/admin', adminRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
